4108f2bfadadb78efa3855e25047cd1a77989717
otto <csiky.otto@aurettoworks.com>
2023-01-24 11:45:01 +0100

    Adds ability to use environmental settings for storing sensitive information

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -3,15 +3,38 @@
 namespace netlab\commercetwo\models;
 
 use craft\base\Model;
+use craft\helpers\App;
+use craft\behaviors\EnvAttributeParserBehavior;
 
 class Settings extends Model
 {
-    public string $merchantId = 'YOUR MERCHANT ID';
+    public ?string $merchantId = null;
+    public ?string $testApiKey = null;
+    public ?string $liveApiKey = null;
     public string $environment = 'test';
     public string $language = 'en_US';
-    public string $testApiKey = 'TEST API KEY';
-    public string $liveApiKey = 'LIVE API KEY';
 
+    public function getMerchantId() :?string {
+        return App::parseEnv($this->merchantId);
+    }
+
+    public function getTestApiKey() :?string {
+        return App::parseEnv($this->testApiKey);
+    }
+
+    public function getLiveApiKey() :?string {
+        return App::parseEnv($this->liveApiKey);
+    }
+
+    public function behaviors(): array
+    {
+        return [
+            'parser' => [
+                'class' => EnvAttributeParserBehavior::class,
+                'attributes' => ['merchantId', 'testApiKey', 'liveApiKey'],
+            ],
+        ];
+    }
 
     public function rules() : array
     {
@@ -19,5 +42,5 @@ class Settings extends Model
             [['merchantId', 'environment', 'language', 'testApiKey', 'liveApiKey'], 'required'],
         ];
     }
-        
+
 }
